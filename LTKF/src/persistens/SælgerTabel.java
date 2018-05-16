33f310486080192a7008fb187af88f37538657aa
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-05-16 12:37:23 +0200

    Lavet tabel klasserne færdig

--- LTKF/src/persistens/SælgerTabel.java
+++ LTKF/src/persistens/SælgerTabel.java
@@ -1,20 +1,40 @@
 package persistens;
 
-public class SælgerTabel implements HasAutoKey{
-private int sælgerId;
-private String initialer;
-private int fastGrænse;
+import java.util.Observable;
 
+public class SælgerTabel extends Observable implements HasAutoKey {
+	private int sælgerId;
+	private String initialer;
+	private int fastGrænse;
 
-public SælgerTabel(int sælgerId, String initialer, int fastGraense) {
-	this.sælgerId=sælgerId;
-	this.initialer=initialer;
-	this.fastGrænse=fastGraense;
-}
-@Override
-public void setAutoKey(int key) {
-  this.sælgerId = key;
-}
+	public SælgerTabel(int sælgerId, String initialer, int fastGrænse) {
+		this.sælgerId = sælgerId;
+		this.initialer = initialer;
+		this.fastGrænse = fastGrænse;
+	}
 
+	@Override
+	public void setAutoKey(int key) {
+		this.sælgerId = key;
+	}
+
+	public int getSælgerId() {
+		return sælgerId;
+	}
+
+	public String getInitialer() {
+		return initialer;
+	}
+
+	public int getFastGrænse() {
+		return fastGrænse;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Sælger: SælgerId=" + sælgerId + 
+			   ", Initialer=" + initialer + 
+			   ", Fastgrænse=" + fastGrænse + "]";
+	}
 
 }
